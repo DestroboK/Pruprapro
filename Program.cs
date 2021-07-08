ac72dc420081caa1a9408b449b0b422748336fac
Kelvin Hernandez <youmater2001@gmail.com>
2021-07-07 22:02:05 -0400

    papa frita

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KelvinyYo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Brian estuvo aqui
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bulin es el mejor y Eric es un come bollo");
+        }
+    }
+}
